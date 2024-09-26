592c01d76be97dc757a9223498345cb46ef11b28
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-26 16:59:36 +0100

    lecture 2, week 3

---
+++ arrays/tutorials/hi.c
@@ -0,0 +1,14 @@
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void) {
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+
+    printf("%c %c %c", c1, c2, c3);
+}
+
+// A string is actually an array.
+
+
