897a348075c6512f200d38e3d2c170a0f2e067e5
olusegun <olusegunbamgbelu755@gmail.com>
2024-11-08 22:09:14 +0100

    using malloc to allocate memory

--- data-structures/list.c
+++ data-structures/list.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (void) 
 {
-    //create an array to take only 3 values
-    int list[3];
+    // allocate memory for an array of 3 integers
+   int *list = malloc(3 * sizeof(int));
+
+   // check if memory allocation is successful
+   if (list == NULL) 
+   {
+        return 1;
+   }
 
     // assign values to the array
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
-    // create a loop to print out each value in the array
+    // print out values 
     for (int i = 0; i < 3; i++) 
     {
         printf("%i\n", list[i]);
     }
+
+    // free memory to avoid memory leaks 
+    free(list);
 }
