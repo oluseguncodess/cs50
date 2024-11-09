3bc6b9ad5aa7d319ddf02896c04d709973678a38
olusegun <olusegunbamgbelu755@gmail.com>
2024-11-09 14:03:03 +0100

    more files added

--- data-structures/list.c
+++ data-structures/list.c
@@ -17,12 +17,31 @@ int main (void)
     list[1] = 2;
     list[2] = 3;
 
-    // print out values 
-    for (int i = 0; i < 3; i++) 
+    // Time passes and i want to add more intergers
+
+    // create a new temporary array and allocate memory for 4 integers
+    int *temp = realloc(list, 4 * sizeof(int));
+
+    // check if memory allocation was successful
+    if (temp == NULL)
+    {
+        free(list);
+        return 1; // exit program as allocation failed
+    }
+
+    // add the last value in the new array
+    temp[3] = 4;
+
+    // rename temp to list
+    list = temp;
+
+    // print out the values 
+    for (int i = 0; i < 4; i++ )
     {
         printf("%i\n", list[i]);
     }
 
-    // free memory to avoid memory leaks 
+    //free memory again to avoid memory leaks
     free(list);
+    return 0;
 }
